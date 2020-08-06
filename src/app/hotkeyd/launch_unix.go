8bb1b74beedcd3a7d9c66cf790dfee6fe6e512f6
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-08-06 18:14:28 +0200

    debug messages for testing on unix

--- src/app/hotkeyd/launch_unix.go
+++ src/app/hotkeyd/launch_unix.go
@@ -9,4 +9,5 @@ import (
 // Launch a program on unix/unix-like platform (not supported)
 func Launch(hidden bool, cmd string, args ...string) {
 	fmt.Println("Unsupported OS, assuming you are just testing. So here is a repsonse!")
+	fmt.Printf("We received the command: %s\nWe were told to pass these arguments: %s\nShould the window be hidden?: %t\n", cmd, args, hidden)
 }
