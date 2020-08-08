d202e1973b06ddbb9aaaabd4729e24abb7dd9b69
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-08-08 17:53:25 +0200

    "toggle" support Ugly workaround but I could not find a better way. I am open for better suggestions with working code!

--- src/app/hotkeyd.go
+++ src/app/hotkeyd.go
@@ -4,17 +4,17 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"os"
 	"runtime"
 	"strings"
 
 	"./hotkeyd"
 	"github.com/MakeNowJust/hotkey"
+	"github.com/kardianos/osext"
 	"tawesoft.co.uk/go/dialog"
 
 	"gopkg.in/ini.v1"
 )
-
-var hotkeysEnabled bool = false
 
 // Make a main function
 func main() {
@@ -25,6 +25,7 @@ func main() {
 		fmt.Printf("OS not supported!\n")
 	}
 
+	// Create hotkeys
 	createHotkeys()
 
 	fmt.Println("Started hotkey loop")
@@ -42,7 +43,7 @@ func createHotkeys() {
 		fmt.Printf("Fail to read file: %v\n", cfgerr)
 		fmt.Println("Using defaults instead")
 
-		dialog.Alert("No hotkeys.ini found, please make one and put some configs in it.\n[name of hotkey]\nModkeys=ctrl+alt\nHotkey=b\nLaunch=C:\\windows\\system32\\notepad.exe\nArgs=--some random -a -r -g -s=here\nHide=false")
+		dialog.Alert("No hotkeys.ini found, please make one and put some configs in it. The undefined Modkeys and Hotkey entries at the very top\nare reserved for the Toggle hotkey!\n\nModkeys=ctrl+alt\nHotkey=q\n[name of hotkey]\nModkeys=ctrl+alt\nHotkey=b\nLaunch=C:\\windows\\system32\\notepad.exe\nArgs=--some random -a -r -g -s=here\nHide=false")
 	} else {
 		for _, hotkeyName := range cfg.SectionStrings() {
 			// When we have a defined hotkey, get the hotkey config
@@ -58,10 +59,8 @@ func createHotkeys() {
 			switch hotkeyName {
 			case "DEFAULT":
 				// If the section is named DEFAULT (or empty which returns DEFAULT)
-				fmt.Println("TODO: hotkeytoggle()")
-
-				// Make a variable to contain our uint32 value for the key combination
-				/*var intKey = hotkey.None
+				// Make an empty intkey
+				var intKey = hotkey.None
 
 				// Convert the modkeys to a hotkey.Modifier
 				intKey = hotkeyd.String2Mod(modKeys)
@@ -72,51 +71,73 @@ func createHotkeys() {
 				// Make our hotkey
 				hkey.Register(intKey, intHotKey, func() {
 					fmt.Println("Hotkey Pressed!")
-					// Execute the defined program
-					hkey.Stop()
-				})*/
+
+					// Get the absolute path to the executable
+					filename, _ := osext.Executable()
+					fmt.Printf(filename)
+
+					// Check if --pause is passed to the arguments
+					switch strings.Contains(strings.Join(os.Args[1:], " "), "--pause") {
+					case false:
+						// Start paused instance
+						hotkeyd.Launch(false, filename, "--pause")
+					case true:
+						// Start normal instance
+						hotkeyd.Launch(false, filename)
+					}
+					// Exit this hotkeyd instance
+					os.Exit(0)
+				})
 
 			default:
-				// If the hotkeys options are not undefined
-				if launchCMD != "" && modKeys != "" && hotKey != "" {
-					// Initialize an empty string slice/array
-					args := []string{}
-					// If the argument string is not empty
-					if cmdArgsStr != "" {
-						// Make a new csv parser to parse the string
-						csv := csv.NewReader(strings.NewReader(cmdArgsStr))
-						// Set the "comma" to whitespace
-						csv.Comma = ' '
+				// For any other hotkey definitions
+				// Check if --pause is passed as an argument
+				switch strings.Contains(strings.Join(os.Args[1:], " "), "--pause") {
+				case false:
+					// If we are not in paused mode
+					// Check if the hotkey options are not undefined
+					if launchCMD != "" && modKeys != "" && hotKey != "" {
+						// Initialize an empty string slice/array
+						args := []string{}
 
-						// Initialize an error variable
-						var csvErr error
+						// If the argument string is not empty
+						if cmdArgsStr != "" {
+							// Make a new csv parser to parse the string
+							csv := csv.NewReader(strings.NewReader(cmdArgsStr))
 
-						// Read the argument string
-						args, csvErr = csv.Read()
+							// Set the "comma" to whitespace
+							csv.Comma = ' '
 
-						// If we get an error
-						if csvErr != nil {
-							// Print the error and return
-							fmt.Println(csvErr)
-							return
+							// Initialize an error variable
+							var csvErr error
+
+							// Read the argument string
+							args, csvErr = csv.Read()
+
+							// If we get an error
+							if csvErr != nil {
+								// Print the error and return
+								fmt.Println(csvErr)
+								return
+							}
 						}
+
+						// Make a variable to contain our uint32 value for the key combination
+						var intKey = hotkey.None
+
+						// Convert the modkeys to a hotkey.Modifier
+						intKey = hotkeyd.String2Mod(modKeys)
+
+						// Get the hotkey from settings and convert to uint32
+						var intHotKey uint32 = hotkeyd.HotkeySwitch(hotKey)
+
+						// Make our hotkey
+						hkey.Register(intKey, intHotKey, func() {
+							fmt.Println("Hotkey Pressed!")
+							// Execute the defined program
+							hotkeyd.Launch(hidewindow, launchCMD, args...)
+						})
 					}
-
-					// Make a variable to contain our uint32 value for the key combination
-					var intKey = hotkey.None
-
-					// Convert the modkeys to a hotkey.Modifier
-					intKey = hotkeyd.String2Mod(modKeys)
-
-					// Get the hotkey from settings and convert to uint32
-					var intHotKey uint32 = hotkeyd.HotkeySwitch(hotKey)
-
-					// Make our hotkey
-					hkey.Register(intKey, intHotKey, func() {
-						fmt.Println("Hotkey Pressed!")
-						// Execute the defined program
-						hotkeyd.Launch(hidewindow, launchCMD, args...)
-					})
 				}
 			}
 		}
